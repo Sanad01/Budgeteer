170802ae69a607321b1c01596a45ec1cb5f96f71
Sanad <sanads945@gmail.com>
2025-05-07 11:00:37 -0700

    Update question_screen.py
    test2

--- app/GUI/question_screen.py
+++ app/GUI/question_screen.py
@@ -33,6 +33,7 @@ class QuestionScreen(QWidget):
         self.master_layout = QVBoxLayout()
 
         self.questions = [
+            "What is your monthly income after taxes? (avg if wages)",
             "How much do you pay for rent?",
             "How much do you pay for utilities? (electric, gas, etc...)",
             "How much do you spend on bills? (internet, phone bill, subs)",
