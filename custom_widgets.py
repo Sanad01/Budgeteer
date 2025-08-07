f73c2741ae10546f37430c6865377844ec07a6a3
Sanad <sanads945@gmail.com>
2025-08-07 12:32:46 -0700

    modified clickable frame

--- custom_widgets.py
+++ custom_widgets.py
@@ -1,13 +1,24 @@
-from PyQt5.QtWidgets import QFrame
+import datetime
+
+from PyQt5.QtWidgets import QFrame, QLabel, QVBoxLayout
 from PyQt5.QtCore import pyqtSignal, QObject, Qt, QEvent
 
 
 class ClickableFrame(QFrame):
     clicked = pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, paycheck_date=None, paycheck_dates=None):
         super().__init__(parent)
         self.selected = None
+        self.paycheck_date = paycheck_date
+        self.paycheck_dates = paycheck_dates or []
+        
+        self.label = QLabel(self)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+        self.update_appearance()
 
     def mousePressEvent(self, a0):
         self.clicked.emit()
@@ -15,6 +26,21 @@ class ClickableFrame(QFrame):
 
     def click(self):
         self.clicked.emit()
+
+    def update_appearance(self):
+        if not self.paycheck_date:
+            return
+
+        # Convert string from DB to datetime.date
+        date_obj = datetime.datetime.strptime(self.paycheck_date, "%Y-%m-%d").date()
+
+        if self.paycheck_date in self.paycheck_dates:
+            self.setStyleSheet("background-color: lightgreen; border: 1px solid green;")
+            self.label.setText(f"💲 {date_obj.strftime('%b %d')}")
+        else:
+            self.setStyleSheet("background-color: lightgray; border: 1px solid #ccc;")
+            self.label.setText(date_obj.strftime('%b %d'))
+
 
 class HoverFilter(QObject):
     HoverEnter = pyqtSignal()
@@ -28,3 +54,6 @@ class HoverFilter(QObject):
             self.HoverLeft.emit()
             print("Hover left button!")
         return super().eventFilter(obj, event)
+
+
+
